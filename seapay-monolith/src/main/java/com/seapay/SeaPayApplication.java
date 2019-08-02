995f94db69eaade64ca7f059c4810a96444bc627
andi.pangeran <andi.pangeran@go-pay.co.id>
2019-08-02 14:56:00 +0700

    [apang, abduh] add empty monolith project

---
+++ seapay-monolith/src/main/java/com/seapay/SeaPayApplication.java
@@ -0,0 +1,14 @@
+package com.seapay;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SeaPayApplication {
+    private static final Logger logger = LoggerFactory.getLogger(SeaPayApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting seapay monolith application");
+
+        logger.info("server is running");
+    }
+}
